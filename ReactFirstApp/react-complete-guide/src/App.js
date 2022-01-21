a8607e2c33fe4326e8b5056a04f3896de0f0d2d6
Marko Radonic <marko.radonic@inform-technologies.com>
2022-01-21 19:35:19 +0100

    015 Splitting Components Into Multiple Components

--- ReactFirstApp/react-complete-guide/src/App.js
+++ ReactFirstApp/react-complete-guide/src/App.js
@@ -1,12 +1,50 @@
-import logo from './logo.svg';
-import ExpenseItem from './components/ExpenseItem';
-import './App.css';
+// import logo from "./logo.svg";
+import ExpenseItem from "./components/ExpenseItem";
+import "./App.css";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 9.54,
+      date: new Date(2022, 1, 21),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 294.54,
+      date: new Date(2022, 1, 21),
+    },
+    { id: "e3", title: "Desk", amount: 194.54, date: new Date(2022, 1, 21) },
+    {
+      id: "e4",
+      title: "Bike Insurance",
+      amount: 394.54,
+      date: new Date(2022, 1, 21),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem/>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
     </div>
   );
 }
