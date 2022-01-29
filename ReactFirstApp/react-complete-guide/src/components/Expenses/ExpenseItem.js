bf079cc09cdfff46ca7233e21478f8a74f4d0224
Marko Radonic <marko.radonic@inform-technologies.com>
2022-01-29 21:52:25 +0100

    009 Wrap Up & Next Steps

--- ReactFirstApp/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ ReactFirstApp/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -5,25 +5,17 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-
-// const [title, setTitle] = useState(props.title); // Sada vise ne citamo odavde na liniji 19 
-
-  // const clickHandler = () => {
-  //    setTitle('Updated');
-  //    console.log(title);
-  // }
   return (
     <li>
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        {/* <h2>{title}</h2> */}
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
-    </Card>
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+      </Card>
     </li>
   );
-}
+};
 
 export default ExpenseItem;
