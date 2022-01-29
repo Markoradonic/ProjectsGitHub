8fff5c22032557e3773f235acca767ef8963e1d8
Marko Radonic <marko.radonic@inform-technologies.com>
2022-01-29 17:47:09 +0100

    006 Adding Conditional Return Statements

--- ReactFirstApp/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ ReactFirstApp/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -13,6 +13,7 @@ const ExpenseItem = (props) => {
   //    console.log(title);
   // }
   return (
+    <li>
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
@@ -21,6 +22,7 @@ const ExpenseItem = (props) => {
         <div className='expense-item__price'>${props.amount}</div>
       </div>
     </Card>
+    </li>
   );
 }
 
